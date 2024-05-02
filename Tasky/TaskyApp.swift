5faf9d1baaaa1adfe5efa33af3baac189cf9e05b
nazmulcuet11 <nazmulcuet11@gmail.com>
2024-05-02 20:16:01 +0700

    initial commit

---
+++ Tasky/TaskyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskyApp.swift
+//  Tasky
+//
+//  Created by Nazmul Islam on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
